8d28075a8147b30d20df2de636a831441a4ac100
Alok pal <alokp@serviots.com>
2025-02-07 11:12:58 +0530

    feat : firebase-android

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, useColorScheme } from 'react-native';
+import React, { useEffect } from 'react';
+import { Alert, View, useColorScheme } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routes from './Navigation/Routes';
@@ -8,6 +8,8 @@ import store from './redux/store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ToastProvider } from './components/customToast/ToastManager';
 import { AuthProvider } from './components/authContext';
+import { PermissionsAndroid } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,6 +18,33 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1,
   };
+
+  useEffect(() => {
+    requestPermissionAndroid()
+  }, [])
+
+  const requestPermissionAndroid = async () => {
+    const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      // Alert.alert("Permission granted")
+      getTokens()
+    } else {
+      Alert.alert("Permission not granted")
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const getTokens = async () => {
+    const fcmToken = await messaging().getToken();
+    console.log("🚀 ~ getTokens ~ fcmToken:", fcmToken)
+  }
 
   return (
     <SafeAreaProvider style={backgroundStyle}>
